05db57990d1354e42306cbfdef83346e5ec43548
Andrew Deitrick <ajdeitrick@gmail.com>
2014-03-13 11:22:28 -0600

    using more specific code to test the copyright data

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'Visiting the home page' do
-  
+
   it "displays all of the menu items" do
     visit "/"
     expect(page).to have_content("Channa Masala")
@@ -10,8 +10,9 @@ describe 'Visiting the home page' do
 
   it "displays the current year in the copyright description" do
     visit "/"
-    expect(page).to have_content(Time.now.year)
+    within('footer') do
+      expect(page).to have_content(2014)
+    end
   end
 
-  
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
 require 'app'
 require 'capybara'
 require 'capybara/dsl'
+require 'capybara/rspec'
 
 Capybara.app = App.new
 
