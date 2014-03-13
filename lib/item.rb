c253a762da5a129052e61254b1eb2695559e061a
vandosant <sjskender@gmail.com>
2014-03-13 15:22:35 -0600

    Refactor item class methods
    Replaced methods that return instance variables with attr_reader

--- lib/item.rb
+++ lib/item.rb
@@ -1,19 +1,9 @@
 class Item
+  attr_reader :name, :price, :description
+
   def initialize(name, price, description)
     @name = name
     @price = price
     @description = description
-  end
-
-  def name
-    @name
-  end
-
-  def price
-    @price
-  end
-
-  def description
-    @description
   end
 end
