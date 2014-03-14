ea393661444523cb3397e767297db6281788e610
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-14 16:11:59 -0600

    Edited the image string to food1.jpg in order for the tests to pass.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,16 +10,20 @@ describe Item do
     expect(item.name).to eq "Channa Masala"
   end
   it 'has a price' do
-    item = Item.new("Channa Masala", "5.95", "Yummy goodness", "food2.jpg")
+    item = Item.new("Channa Masala", "5.95", "Yummy goodness", "food1.jpg")
 
     expect(item.price).to eq "5.95"
   end
   it 'has a description' do
-    item = Item.new("Channa Masala", "5.95", "Yummy goodness", "food3.jpg")
+    item = Item.new("Channa Masala", "5.95", "Yummy goodness", "food1.jpg")
 
     expect(item.description).to eq "Yummy goodness"
   end
+  it 'has an image' do
+    item = Item.new("Channa Masala", "5.95", "Yummy goodness", "food1.jpg")
 
+    expect(item.image).to eq "food1.jpg"
+  end
   it 'knows that two items are the same if they have the same attributes' do
     item1 = Item.new("Channa Masala", "5.95", "Yummy goodness", "food1.jpg")
     item2 = Item.new("Channa Masala", "5.95", "Yummy goodness", "food1.jpg")
