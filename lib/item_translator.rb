7812a298489506b6331ffe6e1183f88ef229f54d
vandosant <sjskender@gmail.com>
2014-03-14 08:07:13 -0600

    Converts a file into an array of hashes

--- lib/item_translator.rb
+++ lib/item_translator.rb
@@ -1,6 +1,7 @@
 class ItemTranslator
   def translate(filename)
-    data = []
+    data = Array.new
+
     filepath = File.expand_path('config/test_menu.csv')
     File.open(filepath, File::RDONLY).each do |info|
       data += info.split(/,/)
@@ -12,6 +13,26 @@ class ItemTranslator
       data[i] = data[i].chomp
       i+= 1
     end
-    data
+
+    all_grouped_items = Array.new
+    grouped_set = Hash.new
+
+    set = 0
+    set_id = 0
+    while set < data.length
+        grouped_set["ID"] = set_id
+        grouped_set["Name"] = data[set]
+        grouped_set["Price"] = data[set+1]
+        grouped_set["Description"] = data[set+2]
+        grouped_set["Image"] = data[set+3]
+
+      all_grouped_items.push(grouped_set)
+        grouped_set = Hash.new
+      set += 4
+      set_id += 1
+    end
+
+    all_grouped_items
+
   end
 end
--- spec/models/item_translator_spec.rb
+++ spec/models/item_translator_spec.rb
@@ -1,15 +1,17 @@
 require 'item_translator'
 
 describe ItemTranslator do
-  it "converts a file to an array of information" do
+  it "converts a file to an array of hashes" do
     info = ItemTranslator.new
 
     actual = info.translate("test_menu.csv")
 
     expected =  [
-                  "name", "price", "description", "image",
-                  "Channa Masala", "5.95", "Yummy goodness", "food1.jpg",
-    ]
+                  {"ID" => 0, "Name" => "name", "Price" => "price",
+                   "Description" => "description", "Image" => "image"},
+                  {"ID" => 1, "Name" => "Channa Masala", "Price" => "5.95",
+                   "Description" => "Yummy goodness", "Image" => "food1.jpg"}
+                ]
 
     expect(actual).to eq expected
   end
