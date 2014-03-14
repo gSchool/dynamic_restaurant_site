f9070893237e449b0df261d86f98e134a9e9b6d2
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-13 19:18:17 -0600

    Refactor a before do into the spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 require 'item'
 
 describe Item do
+  before do
+    @item = Item.new("Channa Masala", "5.95", "Yummy goodness", "food1")
+  end
 
   it 'has an name' do
-    item = Item.new("Channa Masala", "5.95", "Yummy goodness")
-    expect(item.name).to eq "Channa Masala"
+    expect(@item.name).to eq "Channa Masala"
   end
 
   it 'has a price' do
-    item = Item.new("Channa Masala", "5.95", "Yummy goodness")
-    expect(item.price).to eq "5.95"
+    expect(@item.price).to eq "5.95"
   end
 
   it 'has a description' do
-    item = Item.new("Channa Masala", "5.95", "Yummy goodness")
-    expect(item.description).to eq "Yummy goodness"
+    expect(@item.description).to eq "Yummy goodness"
   end
 end
