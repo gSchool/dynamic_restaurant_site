ab9149986a8fd399becf2a6cf4aa30f384f2c7b9
Bebe Peng <bebepeng@gmail.com>
2014-03-17 17:53:17 -0600

    variable menu depending on day of the week
    everyday's menu has multiple items now

--- lib/menu.rb
+++ lib/menu.rb
@@ -9,22 +9,28 @@ class Menu
   def items(day = Date.today)
     a = []
     @spreadsheet.each do |row|
-      if day.wday == day_of_the_week(row["day"])
+      if WdayConverter.new.day_to_num(row["day"]).include?(day.wday)
         a.push(Item.new(row["name"], row["price"], row["description"], row["image"]))
       end
     end
     a
   end
+end
 
-  private
-  def day_of_the_week(day_string)
-    i = 0
-    7.times do
-      if Date::DAYNAMES[i] == day_string
-        return i
+class WdayConverter
+  def day_to_num(days)
+    array_of_days = days.split(",")
+    converted_days_array = []
+    array_of_days.each do |day|
+      i = 0
+      7.times do
+        if Date::DAYNAMES[i] == day
+          converted_days_array.push(i)
+        end
+        i += 1
       end
-      i += 1
     end
+  converted_days_array
   end
 end
 
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 require 'menu'
 
 describe Menu do
-  it "only displays menu items for Wednesday" do
+  it "only displays menu items for Monday" do
     menu = Menu.new(File.expand_path('../dynamic_restaurant_site/config/menu.csv'))
-    wed = Date.new(2014,03,12)
-    expect(menu.items(wed)).to match_array [Item.new("Saag Paneer", "5.95", "Yummy goodness", "food3.jpg")]
+    thurs = Date.new(2014,03,13)
+    expect(menu.items(thurs)).to match_array [Item.new("Channa Masala", "5.95", "Yummy goodness", "food1.jpg"),
+                                            Item.new("Chicken Tikka Masala", "5.95", "Yummy goodness", "food2.jpg"),
+                                            Item.new("Saag Paneer", "5.95", "Yummy goodness", "food3.jpg"),
+                                            Item.new("Alu Gobi", "5.95", "Yummy goodness", "food4.jpg")]
   end
   it "only displays menu items for Thursday" do
     menu = Menu.new(File.expand_path('../dynamic_restaurant_site/config/menu.csv'))
-    thurs = Date.new(2014,03,13)
-    expect(menu.items(thurs)).to match_array [Item.new("Alu Gobi", "5.95", "Yummy goodness", "food4.jpg")]
+    fri = Date.new(2014,03,14)
+    expect(menu.items(fri)).to match_array [Item.new("Chicken Tikka Masala", "5.95", "Yummy goodness", "food2.jpg"),
+                                            Item.new("Saag Paneer", "5.95", "Yummy goodness", "food3.jpg"),
+                                            Item.new("Alu Gobi", "5.95", "Yummy goodness", "food4.jpg"),
+                                            Item.new("Samsosa Chat", "5.95", "Yummy goodness", "food5.jpg")]
   end
 end
 
