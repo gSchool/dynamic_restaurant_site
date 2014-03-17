6d7327cf77ff38ab068fa9d16e03111642d49013
Mike Gehard <mike.gehard@gmail.com>
2014-03-17 11:54:53 -0600

    Load items from a file
    Also make sure that the tests are indpendent from the
    production item file.

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,10 +1,19 @@
 require 'item'
+require 'csv'
 
 class Menu
+  # pass in the file location so we can change
+  # product file without breaking tests.
+  # aka injecting a dependency
+  def initialize(location_of_file)
+    @location_of_file = location_of_file
+  end
+
   def items
-    [
-        Item.new("Channa Masala", 5.95, "Yummy goodness"),
-        Item.new("Chicken Tikka Masala", 5.95, "Yummy goodness")
-    ]
+    result = []
+    CSV.foreach(@location_of_file, {headers: true}) do |row|
+      result << Item.new(row[0], row[1], row[2])
+    end
+    result
   end
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -4,7 +4,10 @@ require 'menu'
 
 describe Menu do
   it 'knows about the items' do
-    menu = Menu.new
+    file_path = File.expand_path('../../test_items.csv', __FILE__)
+    menu = Menu.new(file_path)
+
+
 
     expect(menu.items).to match_array [
                                  Item.new("Channa Masala", 5.95, "Yummy goodness"),
