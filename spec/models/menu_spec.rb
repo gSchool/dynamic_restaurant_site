76fcdfdfb26eb23290680d142265920ee9133a93
Bebe Peng <bebepeng@gmail.com>
2014-03-18 14:28:25 -0600

    changed menu spec to read from a sample menu
    so tests dont break when files change

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -2,21 +2,16 @@ require 'spec_helper'
 require 'menu'
 
 describe Menu do
-  it "only displays menu items for Thursday" do
-    menu = Menu.new(File.expand_path('../dynamic_restaurant_site/config/menu.csv'))
-    thurs = Date.new(2014,03,13)
-    expect(menu.items(thurs)).to match_array [Item.new("Channa Masala", "5.95", "Yummy goodness", "food1.jpg"),
-                                            Item.new("Chicken Tikka Masala", "5.95", "Yummy goodness", "food2.jpg"),
-                                            Item.new("Saag Paneer", "5.95", "Yummy goodness", "food3.jpg"),
-                                            Item.new("Alu Gobi", "5.95", "Yummy goodness", "food4.jpg")]
+  it "only displays menu items for Sunday" do
+    menu = Menu.new(File.expand_path('../dynamic_restaurant_site/config/sample_menu.csv'))
+    sun = Date.new(2014,03,16)
+    expect(menu.items(sun)).to match_array [Item.new("Sunday,Cheeseburger","0.49","baby","sample.jpg")]
   end
   it "only displays menu items for Friday" do
-    menu = Menu.new(File.expand_path('../dynamic_restaurant_site/config/menu.csv'))
+    menu = Menu.new(File.expand_path('../dynamic_restaurant_site/config/sample_menu.csv'))
     fri = Date.new(2014,03,14)
-    expect(menu.items(fri)).to match_array [Item.new("Channa Masala", "5.95", "Yummy goodness", "food1.jpg"),
-                                            Item.new("Saag Paneer", "5.95", "Yummy goodness", "food3.jpg"),
-                                            Item.new("Alu Gobi", "5.95", "Yummy goodness", "food4.jpg"),
-                                            Item.new("Samsosa Chat", "5.95", "Yummy goodness", "food5.jpg")]
+    expect(menu.items(fri)).to match_array [Item.new("Chicken", "1.00", "yum", "sample.jpg"),
+                                            ]
   end
 end
 
